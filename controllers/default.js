462ed2b128b17ac9ca15f7e0d22c885eca2b938c
saper639 <saper639@gmail.com>
2022-09-29 14:40:33 +0800

    Пример обработки контроллеров (GET, POST)

--- controllers/default.js
+++ controllers/default.js
@@ -1,10 +1,26 @@
 exports.install = function() {
     ROUTE('+GET /*'                     );
-    ROUTE('GET /login',      view_login);   
+    ROUTE('GET /login',       view_login);   
+
+    ROUTE('GET  /api/test/{id}',  test_get);   
+    ROUTE('POST /api/test',       test_post);   
 }
 
 function view_login() {
     var self = this;   
     self.sm = F.sitemap('auth');                      
     self.view('page/login');
+}
+
+function test_get(id) {
+  var self = this;
+  console.log(id);
+  console.log(self.query);
+  self.plain('Test get response');
+}
+
+function test_post() {
+  var self = this;
+  console.log(self.body);
+  self.json(SUCCESS('cool'));
 }
