bf53e8f9ae1143848790378cd1e7b8fafd26f506
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2023-01-16 17:37:55 +0300

    Добавление Proxy контроллеров

--- controllers/api.js
+++ controllers/api.js
@@ -4,5 +4,14 @@ exports.install = function() {
     ROUTE('GET    /api/user', 	   			  	      ['*User-->@get']	      );   
     ROUTE('POST   /api/user',  		 			      ['*User-->@save']	      );   
     ROUTE('GET    /api/user/grid',       			  ['*User-->@grid']       );   
-    ROUTE('DELETE /api/user/{id}',      			  ['*User-->@remove']     );   
+    ROUTE('DELETE /api/user/{id}',      			  ['*User-->@remove']     );
+    
+    //proxy
+    PROXY('/cdn/upload',    MAIN.cdn.upload                                   );
+    FILE('/cdn/image/',     img_proxy     ,           ['.jpg', '.jpeg', '.png', '.gif']);
+}
+
+function img_proxy(req, res) {            
+    res.proxy(CONF.cdn.host+'/'+req.path.slice(-2).join('/'), NOOP);
+    return;
 }
--- definitions/app.js
+++ definitions/app.js
@@ -4,7 +4,6 @@ require('sqlagent/mysql').init(CONF.db, CONF.db_log);
 LOCALIZE('/parts/*.html', ['compress']);
 LOCALIZE('/forms/*.html', ['compress']);
 
-//setting const
-MODULE('Utilit').setConstant();
-//setting for company                                                                                                                                                                                                                          
 MODULE('Utilit').setCompany();    
+//выбрать cdn
+MODULE('Utilit').setCDN();  
