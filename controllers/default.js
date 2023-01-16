859c5c075c86f0e7761b55b7fd64bb33f81e5e01
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2023-01-16 16:49:51 +0300

    Добавление запросов get, post

--- controllers/default.js
+++ controllers/default.js
@@ -1,6 +1,9 @@
 exports.install = function() {
     ROUTE('+GET /*'                     );
-    ROUTE('GET /login',      view_login);   
+    ROUTE('GET /login',      view_login);  
+
+    ROUTE('GET  /api/lab3/{id}',  get);   
+    ROUTE('POST /api/lab3',       post); 
 }
 
 function view_login() {
@@ -8,3 +11,16 @@ function view_login() {
     self.sm = F.sitemap('auth');                      
     self.view('page/login');
 }
+
+function get(id) {
+    var self = this;
+    console.log(id);
+    console.log(self.query);
+    self.plain('Successfull getting response');
+  }
+  
+  function post() {
+    var self = this;
+    console.log(self.body);
+    self.json(SUCCESS('success'));
+  }
